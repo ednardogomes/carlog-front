38fa6595c6f875331a6d42caa89a1bf157ac7066
Ednardo Gomes <ednardogomesdev@gmail.com>
2025-10-20 12:28:47 -0300

    style: corrigido bug do icone dentro do input e aumentado um pouco o tamanho do input

--- src/app/components/vehicle/VehicleHeader.tsx
+++ src/app/components/vehicle/VehicleHeader.tsx
@@ -10,14 +10,14 @@ export default function VehicleHeader() {
                 <h1 className="text-2xl font-semibold">Manutenação de Veículos</h1>
                 <p className="text-sm text-slate-500">Registre e acompanhe serviços por veículos</p>
             </div>
-            <div className='grid grid-cols-2 gap-2'>
+            <div className='flex gap-2'>
                 <div className='relative flex items-center bg-white p-2 rounded-md shadow-sm'>
-                    <Search className='absolute flex left-40 h-5 w-5 text-gray-400' />
+                    <Search className='absolute right-3 h-5 w-5 text-gray-400' />
                     <input
                         value={query}
                         onChange={e => setQuery(e.target.value)}
                         placeholder="Procurar veículo..."
-                        className="outline-none w-44"
+                        className="outline-none w-48 pl-3 pr-10 py-2 rounded-md"
                     />
                 </div>
                 <button className='bg-emerald-600 text-white px-4 py-2 rounded-md shadow-sm text-sm cursor-pointer'>Novo Veículo</button>

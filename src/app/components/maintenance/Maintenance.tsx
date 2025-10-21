24ca97feface959d52a50297535f32675098f5cc
Ednardo Gomes <ednardogomesdev@gmail.com>
2025-10-21 16:35:27 -0300

    feat: realizado algumas alterações

--- src/app/components/maintenance/Maintenance.tsx
+++ src/app/components/maintenance/Maintenance.tsx
@@ -1,18 +1,19 @@
-import { Maintenances } from "@/app/types/maintenance";
 import { Vehicle } from "@/app/types/vehicles";
 
 export default function Maintenance({ vehicle }: { vehicle: Vehicle | null }) {
-    const hasNoMaintenance = !vehicle?.maintenances || vehicle.maintenances.length === 0
+    const hasNoMaintenance = vehicle?.maintenances.length === 0
     return (
         <div className="lg:col-span-2 w-full">
             <div className="space-y-3">
-                <h2 className="text-lg front-semibold">Manutenções -- </h2>
+                {vehicle ?
+                    (<h2 className="text-lg font-semibold">Manutenções<span className="mx-2"> &mdash; </span>{vehicle?.make} {vehicle?.model} - {vehicle?.model_year}</h2>) :
+                    (<h2 className="text-lg font-semibold">Manutenções <span className="mx-2"> &mdash; </span> selecione um veículo</h2>)}
                 <div className="flex items-center justify-between mb-4">
                     {hasNoMaintenance ?
                         (<div className="p-6 bg-white rounded-lg text-slate-500">Nenhuma manutenção registrada. Crie a primeira!</div>)
                         :
                         (<div className="grid gap-3 w-full max-h-[15rem] md:max-h-[25rem] overflow-y-auto">
-                            {vehicle.maintenances.map((maintenance) => (
+                            {vehicle?.maintenances.map((maintenance) => (
                                 <div
                                     key={maintenance.id}
                                     className="p-4 bg-white rounded-xl shadow-sm flex flex-col sm:flex-row sm:items-center justify-between gap-3">

6be26e80e4e8b0a5646f1c17474ecb9503099a0a
Ednardo Gomes <ednardogomesdev@gmail.com>
2025-10-31 09:19:09 -0300

    feat: criado lógica para buscar buscar todos os veículos

---
+++ src/app/hooks/vehicle/getVehicles.ts
@@ -0,0 +1,31 @@
+import { useCallback, useEffect, useState } from "react";
+import { getVehiclesService } from "../../services/vehicleService";
+import { Vehicles } from "../../types/vehicles";
+
+interface UseVehiclesResult {
+    vehicles: Vehicles[];
+    loadingVehicles: boolean | null;
+    refetchVehicles: () => Promise<void>;
+}
+
+export function useGetVehicles(): UseVehiclesResult {
+    const [vehicles, setVehicles] = useState<Vehicles[]>([]);
+    const [loadingVehicles, setLoadingVehicles] = useState<boolean | null>(null)
+
+    const refetchVehicles = useCallback(async () => {
+        try {
+            setLoadingVehicles(true)
+            const data = await getVehiclesService();
+            setVehicles(data)
+        } catch (error) {
+            console.error("Error ao buscar veículos:", error);
+            throw error;
+        } finally {
+            setLoadingVehicles(false)
+        }
+    }, []);
+    useEffect(() => {
+        refetchVehicles();
+    }, [refetchVehicles]);
+    return { vehicles, loadingVehicles, refetchVehicles };
+}

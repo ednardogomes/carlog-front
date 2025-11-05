f400541ba302403729028ff88fd7f4ae556f8dfa
Ednardo Gomes <ednardogomesdev@gmail.com>
2025-11-05 20:51:41 -0300

    feat: desenvolvido lógica para tranformar a data de ISO para BR e vice versa

--- src/app/utils/formatDate.tsx
+++ src/app/utils/formatDate.tsx
@@ -1,9 +1,17 @@
-export function formateToBr(isoDateString: string | undefined) {
-    if (!isoDateString) {
+export const formatDateObjectToISOSimple = (date: Date): string => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+};
+
+export const formateToBr = (isoDate: string | undefined): string => {
+    if (!isoDate) {
         return '-';
     }
 
-    const date = new Date(isoDateString);
+    const date = new Date(isoDate);
 
     if (isNaN(date.getTime())) {
         return '-';
@@ -18,3 +26,25 @@ export function formateToBr(isoDateString: string | undefined) {
 
     return formatedDate;
 }
+
+export const formatToISODate = (brlDate: string | undefined): string => {
+    if (brlDate && brlDate.includes('-') && brlDate.length === 10) return brlDate;
+
+    if (!brlDate || brlDate.length !== 10 || brlDate.includes('_')) return '';
+
+    const [day, month, year] = brlDate.split('/');
+    return `${year}-${month}-${day}`;
+};
+
+export const applyDateMask = (value: string): string => {
+    let maskedValue = value.replace(/\D/g, '');
+
+    if (maskedValue.length > 2) {
+        maskedValue = maskedValue.substring(0, 2) + '/' + maskedValue.substring(2);
+    }
+    if (maskedValue.length > 5) {
+        maskedValue = maskedValue.substring(0, 5) + '/' + maskedValue.substring(5);
+    }
+
+    return maskedValue.substring(0, 10);
+};
